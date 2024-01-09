0eb5c0ab66f0f368999a88f60137e008942347e7
MelToc <155629762+MelToc@users.noreply.github.com>
2024-01-09 10:53:22 +0000

    Stand09.01

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -16,3 +16,5 @@ let firstName; // Deklaration (Definition)
 console.log(firstName); // Ausgabe
 firstName = "Max"; // Wertezuweisung /assignment
 console.log(firstName); // Ausgabe 
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!");
