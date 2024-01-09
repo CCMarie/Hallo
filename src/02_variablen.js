11db07ac00487b87cce5f93524e6198fb02ebee3
MelToc <155629762+MelToc@users.noreply.github.com>
2024-01-09 11:26:49 +0000

    Stand 09.02

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -12,9 +12,32 @@
 
 /* Deklaration + Wertezuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertezuweisung /assignment
-console.log(firstName); // Ausgabe 
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertezuweisung / assignment
+// console.log(firstName); // Ausgabe 
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+
+/* 
+   Wertzuweisung II + Datentypen
+   JS ist eine untypisierte Sprache!
+*/
+
+let test; // Deklaration
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------");
+test = "hello"; //string
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------");
+test = 2; // number
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------");
+test = true; // boolean
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+console.log("--------------");
+
