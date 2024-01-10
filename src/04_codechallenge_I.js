016558030670cd4bbff5c71c7d7e6613ea8bd967
CCMarie <155629912+CCMarie@users.noreply.github.com>
2024-01-10 11:20:47 +0000

    04_newNew

---
+++ src/04_codechallenge_I.js
@@ -0,0 +1,22 @@
+
+/*********   CODE CHALLENGE 01 **********/
+ 
+/*
+Geben Sie über eine Prompt-Anforderung eine Zahl ein.
+Geben Sie über eine Prompt-Anforderung eine zweite Zahl ein.
+Berechnen Sie die Summe der beiden Zahlen.
+Geben Sie die Summe der Zahlen in die Konsole aus:
+"Die Summe der Zahlen ist: summe"
+*/
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let zahl1 = parseInt(prompt("Geben Sie eine Zahl ein."));
+let zahl2 = parseInt(prompt("Geben Sie eine zweite Zahl ein."));
+let summe = zahl1 + zahl2;
+console.log("Die Summe der Zahlen ist: " + summe);
+
+
+
+
+
