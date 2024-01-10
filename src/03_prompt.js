e0f1963841963852eda258f7e0e08945f7fb2df0
MelToc <155629762+MelToc@users.noreply.github.com>
2024-01-10 11:20:06 +0000

    10.06

--- src/03_prompt.js
+++ src/03_prompt.js
@@ -8,28 +8,27 @@
 // console.log("Hallo, " + firstName + " " + familyName +"!");
 
 
-const prompt = require('prompt-sync')({sigint: true});
+//const prompt = require('prompt-sync')({sigint: true});
 
 /********** Variante 2 **********/
 // Vorteil: gute Lesbarkeit / aber viele Variablen
 
-let ziffer1, ziffer2, zahl1, zahl2, summe;
+//let ziffer1, ziffer2, zahl1, zahl2, summe;
 
-ziffer1 = prompt("Bitte Zahl 1 eingeben: ");
-ziffer2 = prompt("Bitte Zahl 2 eingeben: ");
+//ziffer1 = prompt("Bitte Zahl 1 eingeben: ");
+//ziffer2 = prompt("Bitte Zahl 2 eingeben: ");
 
-console-log("Dateityp: " + typeof ziffer1);
-console-log("Dateityp: " + typeof ziffer2);
+//console-log("Dateityp: " + typeof ziffer1);
+//console-log("Dateityp: " + typeof ziffer2);
 
 // Typkonvertierung 1: ziffer --> zahl (mit Nachkomma)
-zahl1 = parseFloat(ziffer1);
-zahl2 = parseFloat(ziffer2);
+//zahl1 = parseFloat(ziffer1);
+//zahl2 = parseFloat(ziffer2);
 
-console-log("Dateityp: " + typeof zahl1);
-console-log("Dateityp: " + typeof zahl2);
+//console-log("Dateityp: " + typeof zahl1);
+//console-log("Dateityp: " + typeof zahl2);
 
-summe = zahl1 + zahl2;
+//summe = zahl1 + zahl2;
 
 
-console.log("Die Summe der Zahlen ist: " + summe);
-
+//console.log("Die Summe der Zahlen ist: " + summe);
