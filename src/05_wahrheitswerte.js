e56b2b4dd5430c3f73813f9b7e239505e531a778
CCMarie <155629912+CCMarie@users.noreply.github.com>
2024-01-10 11:33:09 +0000

    04_newnew

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,16 @@
+/* Berechnung Alter */
+
+//deklaration 
+let ageJohn, ageMark
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+
+// Berchnung alter 
+
+ageJohn = 2024 - birthYearJohn;
+ageMark = 2024 - birthYearMark;
+
+// ausgabe
+
+console.log("ageJohn: " + ageJohn );
+console.log("ageMark: " + ageMark );
