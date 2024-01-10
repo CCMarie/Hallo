cf751043604902330d6c683022c707fc498b2498
CCMarie <155629912+CCMarie@users.noreply.github.com>
2024-01-10 09:42:39 +0000

    03_New_prompt

--- src/03_promt.js
+++ src/03_promt.js
@@ -1,1 +1,6 @@
-console.log ("huhu");
+const prompt = require('prompt-sync')({sigint: true});
+// alles muss man selber machen lassen 
+let firstName = prompt("Vorname?: ");
+let familyName = prompt("Name?: ");
+console.log("Hallo, " + firstName + " " + familyName +"!");
+
