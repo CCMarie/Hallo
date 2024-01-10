71105141effb724b98697ca4d6e9a06bd17cd590
MelToc <155629762+MelToc@users.noreply.github.com>
2024-01-10 11:52:13 +0000

    05/ Berechnung Variante 2

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,12 +6,15 @@ let ageJohn, ageMark;
 const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
-// Berechnung Alter //
-ageJohn = 2024 - birthYearJohn;
-ageMark = 2024 - birthYearMark;
+// Berechnung Alter
+let date = new Date(); // Systemfunktion Datum
+let year = date.getFullYear(); // Daten liefern lassen!!
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
 
-// Ausgabe //
+// Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+console.log("Datum: " + date);
 
 
