deeebd15385d057bcdac283a9f401e5741aae761
CCMarie <155629912+CCMarie@users.noreply.github.com>
2024-01-12 11:48:41 +0000

    07_codechelleng_2_12.01.2024new1

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -37,10 +37,10 @@ function ausgabeName() {
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
 // Argumente sind Daten für Parameter
-ausgabeName2("Maria"); //Argument
-ausgabeName2("Max");
-ausgabeName2("Ralf");
-ausgabeName2("Jasmin");
+//ausgabeName2("Maria"); //Argument
+//ausgabeName2("Max");
+//ausgabeName2("Ralf");
+//ausgabeName2("Jasmin");
 
 
 
@@ -56,14 +56,56 @@ function ausgabeName2(firstName) {      //Parameter
 /***** Funktionen 02c *****/
 // Mehrere Parameter| Argumente
 
-ausgabeNameParams("Max" , "Mütze");
+//ausgabeNameParams("Max" , "Mütze");
 
-const prompt = require('prompt-sync')({sigint: true});
+//const prompt = require('prompt-sync')({sigint: true});
 
- function ausgabeName2(firstName, familyName) {      //Parameter
-    
-    
-    console.log("hallo, " + firstName + " " + familyName + "!");
+//ausgabeNameParams(prompt("Vorname? :"),prompt("Name? :")); // Piping
+
+
+function ausgabeNameParams(firstName, familyName) {      //Parameter
+      console.log("hallo, " + firstName + " " + familyName + "!");
     
  }
  
+ /***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+//ausgabeNameSRP("Max" , "Müller")
+
+function ausgabeNameSRP(firstName, familyName) {      //Parameter
+
+    //1. Funktionalität:string composing
+    const GAP = " ";
+    let outputStr = "Hallo, " + firstName + GAP + familyName + "!"
+
+    //2.Funktionalität:string output
+    
+     console.log(outputStr);
+    
+ }
+/***** Funktionen 03b *****/
+
+ //output("hi");
+ //output(2)
+ //output(true);
+
+ //function output(outputDate) {
+ //   console.log(outputDate);
+    // }
+ output(getString("Max" , "Müller"));
+
+ 
+    function getString(firstName, familyName) {
+        const GAP = " ";
+    let outputStr = "hallo, " + firstName + GAP + familyName + "!"
+    return outputStr;
+    
+ }
+
+ function output(outputDate) {
+    console.log(outputDate);
+    
+ }
