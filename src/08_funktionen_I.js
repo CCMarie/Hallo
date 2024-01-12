845f72c3ec538ef89a8ef8aa6a0d51b55cac5863
CCMarie <155629912+CCMarie@users.noreply.github.com>
2024-01-12 10:17:40 +0000

    07_codechelleng_2_12.01.2024new

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -14,3 +14,56 @@ function test()
 {
     console.log("hallo, Jasmin!");
 }
+
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+//Call
+//ausgabeName();
+
+//Funtion
+function ausgabeName() {
+    //Interne Variable | what happens in vegas......
+    let firstName = "Jasmin";
+   // console.log("hallo, Jasmin!");
+   // console.log("hallo, Maria!");
+
+    console.log("hallo, " + firstName + "!");
+}
+ // console.log(firstName); // fehler : Scope
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// Argumente sind Daten für Parameter
+ausgabeName2("Maria"); //Argument
+ausgabeName2("Max");
+ausgabeName2("Ralf");
+ausgabeName2("Jasmin");
+
+
+
+function ausgabeName2(firstName) {      //Parameter
+    
+    
+    console.log("hallo, " + firstName + "!");
+    
+ }
+
+ 
+
+/***** Funktionen 02c *****/
+// Mehrere Parameter| Argumente
+
+ausgabeNameParams("Max" , "Mütze");
+
+const prompt = require('prompt-sync')({sigint: true});
+
+ function ausgabeName2(firstName, familyName) {      //Parameter
+    
+    
+    console.log("hallo, " + firstName + " " + familyName + "!");
+    
+ }
+ 
