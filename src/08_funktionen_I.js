f185afa48070f0ec402c2916c3008ae19efbfeef
CCMarie <155629912+CCMarie@users.noreply.github.com>
2024-01-12 09:15:57 +0000

    07_codechelleng_2_12.01.2024new

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,16 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+//funktionsaufruf /call
+
+//test();
+
+
+//funktionsrumpf 
+//Funtionsdeklaration 
+
+function test()
+{
+    console.log("hallo, Jasmin!");
+}
